7a2a33b7709c4a0fa8d636c3a38f4bbb32a4bef8
alehono <hono.alexandre@gmail.com>
2021-06-28 18:58:44 -0300

    28-06

--- src/Fluxtest.jl
+++ src/Fluxtest.jl
@@ -1,3 +1,25 @@
 using Flux
+using Flux: train!
 
-model = Dense(20, 5)
+actual(x) = 4x + 2
+
+x_train, x_test = hcat(0:5...), hcat(6:10...)
+y_train, y_test = actual.(x_train), actual.(x_test)
+
+opt = Descent()
+
+data = [(x_train, y_train)]
+
+predict = Dense(1, 1)
+
+parameters = params(predict)
+
+loss(x, y) = Flux.Losses.mse(predict(x), y)
+
+for epoch in 1:200
+    train!(loss, parameters, data, opt)
+end
+
+loss(x_train, y_train)
+
+parameters
--- src/PSSR.jl
+++ src/PSSR.jl
@@ -4,7 +4,7 @@ include("./Classifier.jl"), include("./Read.jl")
 
 # Write your package code here.
 
-x, y = read("C:/Users/honoa/IC")
+x, y = read(path)
 X, Y = classifier(x[1], y[1], 3)
 
 end
