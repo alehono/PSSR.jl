d308591742d2daa2d5279b0b9856799fc7b8dffc
alehono <hono.alexandre@gmail.com>
2021-08-06 21:32:15 -0300

    Finished the function that trains the neural nework

--- src/Neural_network.jl
+++ src/Neural_network.jl
@@ -1,18 +1,27 @@
-predictor = Chain(Dense(1, 1), softmax)
+function onecoldencoding(M::Matrix)
+    n = size(M,2)
+    onecold = zeros(n)
+    for sample in 1:n
+        onecold[sample] = findall(M[:,sample] .== maximum(M[:,sample]))[1]
+    end
+    return onecold
+end
 
-function nettrain(predictor, mi::Float64, x_train, y_train, x_test, y_test, niter::Int)
+function accuracy(predictor, dataset)
+    return mean(onecoldencoding(predictor(dataset[1][1])) .== onecoldencoding(dataset[1][2]))
+end
+
+function nettrain(mi::Float64, x_train::Matrix, y_train::Matrix, x_test::Matrix, y_test::Matrix, niter::Int)
 
     traindataset = [(x_train, y_train)] # batch
     testdataset = [(x_test, y_test)]
     opt = Descent(mi)
+    predictor = Chain(Dense(size(x_train, 1), size(y_train, 1), sigmoid), softmax)
     parameters = params(predictor)
     loss(x, y) = Flux.Losses.mse(predictor(x), y)
 
-    train!(loss, parameters, ncycle(traindataset, niter), opt, cb = throttle(() -> println("Training"), 2))
+    train!(loss, parameters, ncycle(traindataset, niter), opt, cb = throttle(() -> println("Training -- ", "Loss: ", loss(x_train, y_train), " train accuracy: ", accuracy(predictor, traindataset), "; test accuracy: ", accuracy(predictor, testdataset)), 2))
 
-    return parameters
+    return predictor, parameters
 end
 
-function accuracy(x, y)
-    return median(predictor(x) == y)
-end
